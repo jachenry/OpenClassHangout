db4e6ebbc022189ca61021fd133000b97ce73abb
Jacob Henry <jachenry@gmail.com>
2013-06-06 18:23:07 -0400

    add hangout module for creating hangouts on the fly

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var phantom = require('phantom');
+var hangout = require('hangout');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
@@ -15,27 +15,20 @@ app.get('/ping', function(req, res) {
   res.send("<h1> Time: " + now + "</h1>");
 });
 
-app.get('/', function(req, res){
-  phantom.create(function(ph) {
-    return ph.createPage(function(page) {
-      return page.open("https://plus.google.com/hangouts/_/", function(status) {
-        console.log("opened google? ", status);
-        res.send('login hit');
-        ph.exit();
-      });
-    });
-  });
-});
-
 app.post('/courses/:course_id/hangout', function(req, res) {
   var courseId = req.params.course_id;
   var requester = req.query.requester;
   var clientId = req.query.clientId;
   var message = "Starting a hangout for course number: " + courseId;
-  var hangoutURL = "https://plus.google.com/hangouts/_/";
+
   console.log(message);
-  io.sockets.emit('hangout_' + courseId, { requester:requester, course_id:courseId, hangout_url:hangoutURL, requester_client_id:clientId });
-  res.status(201).send("Created");
+
+  hangout.create(function(hangoutURL){
+    io.sockets.emit('hangout_' + courseId, { requester:requester, course_id:courseId, hangout_url:hangoutURL, requester_client_id:clientId });
+    res.status(201).send("Created");
+  }, function(){
+    res.status(500).send("Error");
+  })
 });
 
 server.listen(port);
