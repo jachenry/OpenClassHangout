bec30646f01bdeb896f3c02dbc92b34cc9666c57
Tony Hillerson <tony.hillerson@gmail.com>
2013-06-06 09:53:18 -0600

    A simple hangout route

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
 
 app.get('/', function(req, res){
   res.send('Hello World!');
 });
 
-app.listen(8080);
-console.log('Listening on port 8080');
+app.post('/courses/:course_id/hangout', function(req, res) {
+  console.log("Starting a hangout for course number: ");
+  console.log(req.params);
+});
+
+var port = 8080;
+app.listen(port);
+console.log('Listening on port ' + port);
